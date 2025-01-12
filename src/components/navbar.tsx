d5b962f5034b17b61ac38cbc66ee40e9441ef6f4
Aufa Syaihan Azzahidi <aufa.azzahidi@gmail.com>
2025-01-12 11:23:44 +0700

    changed navbar and main content

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,29 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import logo from "@/assets/logo.png";
-import Image from "next/image";
 
 export default function Navbar() {
     return (
-        <nav className="w-full bg-blue-900 py-2 px-6 font-[family-name:var(--font-poppins)]">
+        <nav className="w-full bg-blue-900 py-4 px-6 font-[family-name:var(--font-poppins)]">
             <div className="flex gap-4 items-center justify-between w-full">
-                <Link href="/" className="flex gap-4 items-center">
-                    <Image
-                        src={logo.src}
-                        alt="Logo"
-                        width={50}
-                        height={50}
-                        className="rounded-full"
-                    />
+                <Link href="/">
                     <h1 className="text-xl font-bold">YourAnimeList</h1>
                 </Link>
-                <ul className="flex gap-8">
+                <ul className="flex gap-2">
                     <li>
-                        <Link href="/animes">Animes</Link>
+                        <Link href="/animes" className="hover:bg-blue-700 px-4 py-2 rounded-md">Animes</Link>
                     </li>
                     <li>
-                        <Link href="/animes/share">Share Animes</Link>
+                        <Link href="/animes/share" className="hover:bg-blue-700 px-4 py-2 rounded-md">Share Animes</Link>
                     </li>
                 </ul>
             </div>
