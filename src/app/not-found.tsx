9625a2dd0a63866f4c44f6ac9b809c7a0d4d2495
Aufa Azzahidi <aufa.azzahidi@gmail.com>
2025-02-20 15:51:10 +0700

    created not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound: React.FC = () => {
+    return (
+        <div className="flex items-center justify-center h-full">
+            <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+        </div>
+    );
+}
+
+export default NotFound;
