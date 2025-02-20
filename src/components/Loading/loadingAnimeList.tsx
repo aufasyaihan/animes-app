20de69e45ca739c670118a020bbf6a0b27ab7f5a
Aufa Azzahidi <aufa.azzahidi@gmail.com>
2025-02-20 16:37:18 +0700

    improve loading page

--- src/components/Loading/loadingAnimeList.tsx
+++ src/components/Loading/loadingAnimeList.tsx
@@ -1,11 +1,11 @@
 const LoadingAnimeList: React.FC = () => {
     return (
-        <div className="grid grid-cols-5 justify-between gap-4 w-full mx-auto pb-8">
-            <div className="w-full h-[300px] bg-gray-600 animate-pulse"></div>
-            <div className="w-full h-[300px] bg-gray-600 animate-pulse"></div>
-            <div className="w-full h-[300px] bg-gray-600 animate-pulse"></div>
-            <div className="w-full h-[300px] bg-gray-600 animate-pulse"></div>
-            <div className="w-full h-[300px] bg-gray-600 animate-pulse"></div>
+        <div className="grid grid-cols-5 justify-between gap-4 w-full mx-auto pb-8 animate-pulse">
+            <div className="w-full h-[300px] bg-gray-600"></div>
+            <div className="w-full h-[300px] bg-gray-600"></div>
+            <div className="w-full h-[300px] bg-gray-600"></div>
+            <div className="w-full h-[300px] bg-gray-600"></div>
+            <div className="w-full h-[300px] bg-gray-600"></div>
         </div>
     );
 };
--- src/components/Loading/loadingCarousel.tsx
+++ src/components/Loading/loadingCarousel.tsx
@@ -1,14 +1,14 @@
 const LoadingCarousel: React.FC = () => {
     return (
-        <div className="flex flex-col gap-4 w-full mt-4 mx-auto items-center sm:items-start">
+        <div className="flex flex-col gap-4 w-full mt-4 mx-auto items-center sm:items-start animate-pulse">
             <div className="w-full flex gap-2">
-                <div className="w-full h-[350px] bg-gray-600 animate-pulse"></div>
-                <div className="w-full h-[350px] bg-gray-600 animate-pulse"></div>
-                <div className="w-full h-[350px] bg-gray-600 animate-pulse"></div>
-                <div className="w-full h-[350px] bg-gray-600 animate-pulse"></div>
-                <div className="w-full h-[350px] bg-gray-600 animate-pulse"></div>
+                <div className="w-full h-[350px] bg-gray-600"></div>
+                <div className="w-full h-[350px] bg-gray-600"></div>
+                <div className="w-full h-[350px] bg-gray-600"></div>
+                <div className="w-full h-[350px] bg-gray-600"></div>
+                <div className="w-full h-[350px] bg-gray-600"></div>
             </div>
-            <div className="w-64 h-5 bg-gray-600 mx-auto animate-pulse"></div>
+            <div className="w-64 h-5 bg-gray-600 mx-auto"></div>
         </div>
     );
 };
